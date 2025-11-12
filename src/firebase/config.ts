0b2509990aaa3f308c6db3872a94c1e0d2aca7e2
Vachaspati Aman <vachaspatiaman4@gmail.com>
2025-11-12 11:07:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8126400486-18510",
+  "appId": "1:542162390995:web:a1bceeb44bf08ea3002926",
+  "apiKey": "AIzaSyAl9Rq-_jCmZeLA90zLUJHN2-c1bj4J9r8",
+  "authDomain": "studio-8126400486-18510.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "542162390995"
+};
